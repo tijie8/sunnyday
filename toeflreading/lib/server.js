04ae4a985b2a4441de31b04eada299dd5b9d37b5
Nicholas <nicholas.zhaoyu@gmail.com>
2015-02-27 06:41:00 -0800

    Create lib/server.js, prepare for webdriver test.

---
+++ toeflreading/lib/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var render = require('./render').render;
+var router = require('./routers').router;
+
+var koa = require('koa');
+var serve = require('koa-static');
+
+var app = koa();
+
+render(app);
+
+app
+  .use(router.routes())
+  .use(serve('static'))
+  .use(router.allowedMethods());
+
+module.exports = app;
+
--- toeflreading/server.js
+++ toeflreading/server.js
@@ -1,17 +1,6 @@
 'use strict';
 
-var koa = require('koa');
-var render = require('./lib/render').render;
-var serve = require('koa-static');
-var router = require('./lib/routers').router;
-
-var app = koa();
-
-render(app);
-
-app
-  .use(router.routes())
-  .use(serve('static'))
-  .use(router.allowedMethods());
+var app = require('./lib/server');
 
 app.listen(8080);
+
